9c7feaaed0f79a801bb07d4a2f6f798afc284045
woofdoggo <woofwoofdoggo@protonmail.com>
2022-10-17 20:54:53 -0400

    get_inputs: truncate trailing newline

--- get_inputs.sh
+++ get_inputs.sh
@@ -3,4 +3,5 @@
 mkdir -p $1/inputs
 for i in $(seq 1 25); do
     curl -H "Cookie: $2" https://adventofcode.com/$1/day/$i/input --output $1/inputs/$i
+    truncate -s -1 $1/inputs/$i # remove trailing newline
 done
