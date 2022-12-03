832ee5f82a743273cedbadfd15a849511d69a4f6
woofdoggo <woofwoofdoggo@protonmail.com>
2022-12-03 10:43:34 -0500

    get_input: use env var as token

--- get_input.sh
+++ get_input.sh
@@ -2,6 +2,11 @@
 
 mkdir -p $1/inputs
 filename="$1/inputs/$(printf %02d $2)"
-curl -A "github.com/woofdoggo (woofwoofdoggo@protonmail.com : read infrequently)" -H "Cookie: $3" https://adventofcode.com/$1/day/$2/input --output $filename
+
+curl -A "github.com/woofdoggo (woofwoofdoggo@protonmail.com : read infrequently)" \
+    -H "Cookie: session=$AOC_TOKEN" \
+    https://adventofcode.com/$1/day/$2/input \
+    --output $filename
+
 # remove trailing newline
 truncate -s -1 $filename
