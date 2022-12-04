2302ee7db396adce4641825d28d730c6e6820cdf
woofdoggo <woofwoofdoggo@protonmail.com>
2022-12-04 13:50:28 -0500

    2022: refactor day 3 and 4

--- 2022/04.py
+++ 2022/04.py
@@ -1,26 +1,20 @@
+import util
 from sys import argv
 
 F = [l.strip() for l in open(argv[1])]
 S = 0
 
 for l in F:
-    h = l.split(",")
-    c = h[0].split("-")
-    d = h[1].split("-")
-    a, b, x, y = int(c[0]), int(c[1])+1, int(d[0]), int(d[1])+1
-    i, j = set(range(a, b)), set(range(x, y))
-    if i <= j or j <= i:
+    a, b, c, d = util.scan("%d-%d,%d-%d", l)
+    x, y = set(util.irange(a,b)), set(util.irange(c,d))
+    if x <= y or y <= x:
         S += 1
 print(S)
 
 S = 0
 for l in F:
-    h = l.split(",")
-    c = h[0].split("-")
-    d = h[1].split("-")
-    a, b, x, y = int(c[0]), int(c[1])+1, int(d[0]), int(d[1])+1
-    i, j = set(range(a, b)), set(range(x, y))
-    a = list(set(i) & set(j))
-    if len(a) > 0:
+    a, b, c, d = util.scan("%d-%d,%d-%d", l)
+    x, y = set(util.irange(a,b)), set(util.irange(c,d))
+    if x & y:
         S += 1
 print(S)
