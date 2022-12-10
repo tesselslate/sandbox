6dea115cb999fa23bc52588ddf6d2bc4b6f8c6ed
woofdoggo <woofwoofdoggo@protonmail.com>
2022-12-10 01:10:35 -0500

    2022: day 10 rewrite
    bleh

--- 2022/10.py
+++ 2022/10.py
@@ -4,46 +4,31 @@ from functools import cache
 from sys import argv
 
 F = [l.strip() for l in open(argv[1])]
-F.append("noop")
 
 X = 1
-C = 1
+C = 0
 S = 0
+G = util.grid(40, ".")
 
-G = util.grid(40, 0)
-
-ip = 0
-addx = None
-while ip < len(F) or addx != None:
+def update():
+    global G, S
     if (C - 20) % 40 == 0:
         S += X*C
-
-    # crt draw
-    b = False
     for i in range(-1, 2):
         cx = (C-1) % 40
         if cx == X+i:
-            b = True
-            G[C//40][cx] = 1
+            G[C//40][cx] = "#"
 
-    if addx != None:
-        X += addx
-        addx = None
-        C += 1
-        continue
-
-    C += 1
-
-    w = F[ip].split(" ")
-    ip += 1
+for l in F:
+    w = l.split(" ")
     if w[0] == "noop":
-        continue
-    if w[0] == "addx":
-        addx = int(w[1])
-        continue
+        C += 1
+        update()
+    else:
+        C += 1
+        update()
+        C += 1
+        update()
+        X += int(w[1])
+util.print_grid(G)
 print(S)
-for y in G[:6]:
-    s = ""
-    for x in y:
-        s += "#" if x else "."
-    print(s)
