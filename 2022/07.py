6c0d967d2aac3a375d2464bee3aa067ad2e6f585
woofdoggo <woofwoofdoggo@protonmail.com>
2022-12-09 22:11:21 -0500

    2022: day 7 cleanup

--- 2022/07.py
+++ 2022/07.py
@@ -1,44 +1,41 @@
+import collections
 from sys import argv
 
 F = [l.strip() for l in open(argv[1])]
-S = 0
 
 cd = []
 fs = {}
 
-i = -1
+def get_dir(dir):
+    c = fs
+    for d in dir:
+        c = c[d]
+    return c
+
+i = 0
 while i < len(F):
-    i += 1
     l = F[i]
-    words = l.split(" ")
-    if words[0] == '$' and words[1] == 'cd':
-        dir = words[2]
-        if dir == '/':
-            cd = []
-        elif dir == '..':
-            cd.pop()
-        else:
-            cd.append(dir)
-        continue
-    if words[0] == '$' and words[1] == 'ls':
-        i += 1
-        entries = {}
-        while i < len(F):
-            l = F[i]
-            if F[i][0] == '$':
-                i -= 1
-                break
+    if l[0] == '$':
+        cmd = l.split(" ")[1:]
+        if cmd[0] == "ls":
             i += 1
-            words = l.split(" ")
-            if words[0] == 'dir':
-                entries[words[1]] = {}
+            while i < len(F) and F[i][0] != '$':
+                line = F[i]
+                words = line.split(" ")
+                if words[0] == "dir":
+                    get_dir(cd)[words[1]] = {}
+                else:
+                    get_dir(cd)[words[1]] = int(words[0])
+                i += 1
+            continue
+        elif cmd[0] == "cd":
+            if cmd[1] == '/':
+                cd = []
+            elif cmd[1] == '..':
+                cd.pop()
             else:
-                entries[words[1]] = int(words[0])
-        c = fs
-        for dir in cd:
-            c = c[dir]
-        for k, v in entries.items():
-            c[k] = v
+                cd.append(cmd[1])
+    i += 1
 
 S = 0
 T = 0
@@ -54,22 +51,19 @@ def recurse(d):
     if s <= 100000:
         S += s
     return s
-
 recurse(fs)
 print(S)
-U = 70000000 - T
 
 deletable = {}
-def recurse2(cd, d):
-    global U
+def recurse2(d, cd="/"):
     s = 0
-    for k,v in d.items():
+    for k, v in d.items():
         if type(v) == int:
             s += v
         else:
-            s += recurse2(cd + k + "/", v)
-    if s + U >= 30000000:
+            s += recurse2(v, cd+k+"/")
+    if T - s <= 40000000:
         deletable[cd] = s
     return s
-recurse2("/", fs)
-print(deletable[min(deletable, key=deletable.get)])
+recurse2(fs)
+print(deletable[min(deletable, key=deletable.get)]) # type: ignore
