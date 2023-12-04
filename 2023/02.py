055e99c90760293014e4b2b32079513801e787bb
tesselslate <46545045+tesselslate@users.noreply.github.com>
2023-12-04 16:28:54 -0500

    2023: rewrite 2

--- 2023/02.py
+++ 2023/02.py
@@ -11,45 +11,17 @@ F = [l.strip() for l in F]
 while F[-1] == "":
     del F[-1]
 
-S = 0
-
-R = 12
-G = 13
-B = 14
-for i, l in enumerate(F):
-    n, b = ul.scan("Game %d: %s", l)
-    good = True
-    for game in b.split(";"):
-        for w in game.split(","):
-            w = w.strip().split()
-            x = int(w[0])
-            t = w[1]
-            if t == "blue" and x > B:
-                good = False
-            if t == "red" and x > R:
-                good = False
-            if t == "green" and x > G:
-                good = False
-    if good:
-        S += i+1
-print(S)
-
-S = 0
-for i, l in enumerate(F):
-    n, b = ul.scan("Game %d: %s", l)
-    good = True
-    mr,mg,mb=0,0,0
-    for game in b.split(";"):
-        for w in game.split(","):
-            w = w.strip().split()
-            x = int(w[0])
-            t = w[1]
-            if t == "blue" and x > mb:
-                mb = x
-            if t == "red" and x > mr:
-                mr = x
-            if t == "green" and x > mg:
-                mg = x
-    if good:
-        S += mr*mg*mb
-print(S)
+S, S2 = 0, 0
+for l in F:
+    n, g = ul.scan("Game %d: %s", l)
+    pos = True
+    cubes = defaultdict(int)
+    for nc, nt in ul.batched(g.replace(',','').replace(';','').strip().split(), 2):
+        match (int(nc), nt):
+            case (nc, "red") if nc > 12: pos = False
+            case (nc, "green") if nc > 13: pos = False
+            case (nc, "blue") if nc > 14: pos = False
+        cubes[nt] = max(cubes[nt], nc)
+    if pos: S += n
+    S2 += math.prod(cubes.values())
+print(S, S2)
