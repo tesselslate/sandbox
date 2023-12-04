e2c0d25dbca4fbe55c57865bf27b393d9f6a365e
tesselslate <46545045+tesselslate@users.noreply.github.com>
2023-12-04 16:10:28 -0500

    2023: rewrite 4

--- 2023/04.py
+++ 2023/04.py
@@ -12,59 +12,15 @@ while F[-1] == "":
     del F[-1]
 
 S = 0
-
-i = 0
-
-winning = defaultdict(bool)
-count = defaultdict(int)
+c = [0 for _ in F]
 for l in F:
-    n, b = ul.scan("Card %d: %s", l)
-    a, b = b.split("|")
-    winning_ = []
-    sum = 0
-    for x in a.strip().split():
-        winning_.append(int(x))
-    won = False
-    for x in b.strip().split():
-        if int(x) in winning_:
-            # if sum == 0:
-            #     sum = 1
-            # else:
-            #     sum *= 2
-            sum += 1
-            won = True
-    if won:
-        winning[n] = True
-        count[n] = sum
-    if won:
-        assert sum > 0
-        S += 2 ** (sum-1)
+    n, w, h = ul.scan("Card %d: %s| %s", l)
+    w = set([int(w.strip()) for w in w.split()])
+    h = set([int(h.strip()) for h in h.split()])
+    v = len(w&h)
+    if v:
+        S += 2 ** (v-1)
+        for i in range(n, n+v):
+            c[i] += c[n-1] + 1
 print(S)
-
-# S = 0
-# @functools.cache
-# def re(x):
-#     v = 0
-#     if count[x]: v += count[x]
-#     for i in range(count[x]):
-#         v += re(i+x+1)
-#     print(x,v)
-#     return v
-# for k, v in winning.items():
-#     S += re(k)
-# print(S)
-
-S = 0
-V = defaultdict(int)
-def win(card):
-    V[card] += 1
-    v = count[card]
-    for i in range(count[card]):
-        v += win(i+card+1)
-    return v
-for k in winning.keys():
-    win(k)
-
-for x in V.values():
-    S += x
-print(S)
+print(sum(c) + len(c))
