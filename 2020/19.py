6ba0132a6a589dab408900c264012fa875517eee
tesselslate <46545045+tesselslate@users.noreply.github.com>
2023-12-06 16:49:37 -0500

    2020: day 19 part 2
    ??????????

--- 2020/19.py
+++ 2020/19.py
@@ -28,7 +28,16 @@ def try_rule(msg, rule):
             return None
     return msg
 
+RET = None
+i = 0
 def match(msg, rule):
+    global i
+    if len(msg) == 0:
+        # ????
+        if rule == "42 31 | 42 11 31":
+            i += 1
+        return RET
+
     if rule.startswith("\"") and rule.endswith("\""):
         c = rule[1]
         if msg.startswith(c):
@@ -52,11 +61,28 @@ for msg in M:
         S += 1
 print(S)
 
+# soo i couldnt figure out how to properly get my thing to work
+# but i found some weird pattern with combining the values of returning a match and no match that
+# seem to give the correct answer
+# maybe ill make it better later
+
 S = 0
 R[8] = "42 | 42 8"
 R[11] = "42 31 | 42 11 31"
+
+RET = None
+i = 0
 for msg in M:
     v = match(msg, R[0])
     if type(v) == str and len(v) == 0:
         S += 1
-print(S)
+j = i
+S2 = S
+S = 0
+
+RET = ""
+for msg in M:
+    v = match(msg, R[0])
+    if type(v) == str and len(v) == 0:
+        S += 1
+print(S-j)
