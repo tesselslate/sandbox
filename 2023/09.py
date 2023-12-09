d212d7cbb88cbb29206fb4469914eb3d6d3df78b
tesselslate <46545045+tesselslate@users.noreply.github.com>
2023-12-09 00:21:14 -0500

    2023: day 9
    11:55/12:23 | lost an ungodly amount of time because my ints function
    did not take negatives. i'm done using it at this point because it's
    lost me a ton of time on literally every day i've tried to use it, so
    stupid. had an almost working solution for part 1 at ~3m and then spent
    the next ~8 minutes trying to figure out why it no worky (there was one
    other small bug besides not having negatives, but figuring out that my
    parsed input had no negatives was the main bottleneck)
    
    also exactly #999 on part 2 which is funny

---
+++ 2023/09.py
@@ -0,0 +1,39 @@
+import functools, math, re, string, sys, itertools, ul
+from dataclasses import dataclass
+from collections import Counter, defaultdict, deque
+
+if len(sys.argv) > 1:
+    F = open(sys.argv[1])
+else:
+    F = sys.stdin.readlines()
+
+F = [l.strip() for l in F]
+while F[-1] == "":
+    del F[-1]
+
+F = [[int(y) for y in x.split()] for x in F]
+
+S = 0
+
+for l in F:
+    histories = [l.copy()]
+    while not all(x == 0 for x in histories[-1]):
+        xs = []
+        for (a, b) in itertools.pairwise(histories[-1]):
+            xs.append(b-a)
+        histories.append(xs)
+    for x in histories:
+        S += x[-1]
+print(S)
+
+S = 0
+for l in F:
+    histories = [l.copy()]
+    while not all(x == 0 for x in histories[-1]):
+        xs = []
+        for (b, a) in itertools.pairwise(histories[-1]):
+            xs.append(b-a)
+        histories.append(xs)
+    for x in histories:
+        S += x[0]
+print(S)
