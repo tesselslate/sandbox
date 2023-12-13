c3849d5374dbb648dbb689f1fb1a89befc97d70e
tesselslate <46545045+tesselslate@users.noreply.github.com>
2023-12-13 18:21:58 -0500

    2023: rewrite 13

--- 2023/13.py
+++ 2023/13.py
@@ -10,56 +10,25 @@ else:
 F = [l.strip() for l in F]
 while F[-1] == "":
     del F[-1]
-F = ul.double_linefeed(F)
-G = [ul.grid(g) for g in F]
+G = [ul.grid(g) for g in ul.double_linefeed(F)]
+
+def ndiff(g, r):
+    x = min(r, len(g)-r)
+    return sum(sum(g[r-rr-1][c] != g[r+rr][c] for c in range(len(g[0]))) for rr in range(x))
+
+def diffs(g):
+    return [ndiff(g, i) for i in range(1, len(g))]
+
+def value(diffs, vert, smudges):
+    i = diffs.index(smudges) + 1 if smudges in diffs else 0
+    return i * (100 if vert else 1)
 
 S = 0
-
-def check(g, r):
-    n = min(r, len(g)-r)
-    a = g[r-n:r]
-    b = g[r:r+n]
-    assert len(a) == len(b)
-    assert len(a[0]) == len(b[0])
-    b.reverse()
-    return a == b
-
-V = []
+S2 = 0
 for g in G:
-    for i in range(1,len(g)):
-        if check(g, i):
-            S += i * 100
-            V.append((i,"v"))
-            break
-    else:
-        for i in range(1,len(g[0])):
-            if check(ul.transpose(g), i):
-                S += i
-                V.append((i,"h"))
-                break
-        else:
-            assert False
+    a = diffs(g)
+    b = diffs(ul.transpose(g))
+    if (v := value(a, True, 0)) or (v := value(b, False, 0)): S += v
+    if (v := value(a, True, 1)) or (v := value(b, False, 1)): S2 += v
 print(S)
-
-S = 0
-for (g, valid) in zip(G, V):
-    for (r,c) in ul.gridpoints(g):
-        g[r][c] = "." if g[r][c] == "#" else "#"
-        v = []
-        for i in range(1, len(g)):
-            if check(g, i):
-                v.append((i,"v"))
-        for i in range(1,len(g[0])):
-            if check(ul.transpose(g), i):
-                v.append((i,"h"))
-        go = False
-        g[r][c] = "." if g[r][c] == "#" else "#"
-        if len(v):
-            v = [x for x in v if x != valid]
-            if len(v) == 1:
-                v = v[0]
-                S += v[0] * (1 if v[1] == "h" else 100)
-                go = True
-        if go: break
-    else: assert False
-print(S)
+print(S2)
