30b515600d187d64c8cb3a7c113b917a71f44b40
tesselslate <46545045+tesselslate@users.noreply.github.com>
2023-12-13 18:44:00 -0500

    template: use ul.input

--- template.py
+++ template.py
@@ -1,17 +1,8 @@
-import functools, math, re, string, sys, itertools, ul
+import functools, math, re, string, itertools, ul
 from dataclasses import dataclass
 from collections import Counter, defaultdict, deque
 
-if len(sys.argv) > 1:
-    F = open(sys.argv[1])
-else:
-    F = sys.stdin.readlines()
-
-F = [l.strip() for l in F]
-while F[-1] == "":
-    del F[-1]
-
-S = 0
+F = ul.input()
 
 for l in F:
     print(l)
