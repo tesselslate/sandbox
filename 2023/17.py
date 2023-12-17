1f6d961b76770cfff2fde39a0da44a1a4f750421
tesselslate <46545045+tesselslate@users.noreply.github.com>
2023-12-17 12:03:14 -0500

    2023: rewrite 17

--- 2023/17.py
+++ 2023/17.py
@@ -1,37 +1,33 @@
-import functools, math, re, string, itertools, ul
-from dataclasses import dataclass
-from collections import Counter, defaultdict, deque
-import heapq
+import math, ul
+from collections import defaultdict
 
 F = ul.input()
 G = ul.grid(F, int)
 
-Q = [(0, (0,0), "E", 0, ((0, 0)))]
-V = set()
-M = {}
-while len(Q):
-    (cost, (r, c), dir, dirc, path) = heapq.heappop(Q)
-    k = (r,c,dir,dirc)
-    if k not in V:
-        V.add(k)
-        if (r,c) == (len(G)-1, len(G[0])-1) and dirc >= 4:
-            print(cost)
-            exit()
+dirs = ul.dirs_rc("NESW")
 
-        #nextdirs = dir if dirc < 3 else ""
-        nextdirs = dir if dirc < 10 else ""
-        if dirc >= 4:
-            if dir in "NS": nextdirs += "EW"
-            if dir in "EW": nextdirs += "NS"
-        dirs = ul.dirs_rc(nextdirs)
-        for ch, d in zip(nextdirs, dirs):
-            ndirc = 1 if ch != dir else dirc+1
+def mincost(mi, ma):
+    Q = ul.pq([(0, 0, 0, d, 0) for d in ((0,1),(1,0))])
+    V = set()
+    M = defaultdict(lambda: math.inf)
+
+    while len(Q):
+        (cost, r, c, dir, n) = Q.pop()
+        if (r, c, dir, n) in V: continue
+        V.add((r, c, dir, n))
+        if (r, c) == (len(G)-1, len(G[0])-1) and n >= mi:
+            return cost
+
+        di = dirs.index(dir)
+        dd = [dir] if n < ma else []
+        if n >= mi: dd += [dirs[di-1], dirs[(di+1)%4]]
+        for d in dd:
             rr, cc = r + d[0], c + d[1]
-            if not ul.gridcheck(G,rr,cc): continue
-            ncost = cost + G[rr][cc]
-            k = (rr,cc,ch,ndirc)
-            if k not in M or ncost < M[k]:
-                M[k] = ncost
-                heapq.heappush(Q, (ncost, (rr, cc), ch, ndirc, (*path, (rr, cc))))
-
-                # 953 984 1079 bad
+            if not ul.gridcheck(G, rr, cc): continue
+            nc = cost + G[rr][cc]
+            nd = n+1 if d == dir else 1
+            if nc < M[rr, cc, d, nd]:
+                M[rr, cc, d, nd] = nc
+                Q.push((nc, rr, cc, d, nd))
+print(mincost(1,3))
+print(mincost(4,10))
