02860ad4ae89c8be7e41e3baff5ed4e854cecb3b
tesselslate <46545045+tesselslate@users.noreply.github.com>
2023-12-20 17:16:05 -0500

    2023: rewrite 20
    i did part 2 on my phone and it was not very good so i never committed it

--- 2023/20.py
+++ 2023/20.py
@@ -1,61 +1,48 @@
-import functools, math, re, string, itertools, ul
-from dataclasses import dataclass
-from collections import Counter, defaultdict, deque
+import itertools, math, ul
+from collections import deque
 
 F = ul.input()
-G = {}
 
-LO = False
-HI = True
+LO, HI = False, True
 
+M = {}
 for l in F:
-    a, xs = ul.scan("%s -> %s", l)
-    G[a] = [x.strip() for x in xs.split(",")]
+    name, outputs = ul.scan("%s -> %s", l)
+    M[name] = [x.strip() for x in outputs.split(",")]
+S = {k[1:]: {} if k.startswith("&") else LO for k in M}
+S['rx'] = None
+for k, outputs in M.items():
+    for x in outputs:
+        if type(S[x]) == dict: S[x][k[1:]] = LO
+for k in list(M.keys()):
+    M[k[1:]] = M[k]
+    del M[k]
 
-S_F = {k[1:]: False for k in G if k.startswith("%")}
-S_C = {k[1:]: {} for k in G if k.startswith("&")}
-for k, xs in G.items():
-    for x in xs:
-        if x in S_C: S_C[x][k[1:]] = LO
-
-ks = list(G.keys())
-for k in ks:
-    if k[0] == "&" or k[0] == "%":
-        G[k[1:]] = G[k]
-        del G[k]
+f = [k for k, v in M.items() if "rx" in v][0]
+C = {k: -1 for k, v in M.items() if f in v}
 
 lo, hi = 0, 0
-v = set()
-for i in itertools.count():
-    if i == 1000: print("part 1:", lo * hi)
-    rx_lo, rx_hi = 0, 0
-
-    pulses = deque([(None, "broadcaster", LO)])
-    while len(pulses):
-        (sender, module, ptype) = pulses.popleft()
+for i in itertools.count(1):
+    Q = deque([(None, "roadcaster", LO)])
+    while len(Q):
+        (sender, module, ptype) = Q.popleft()
         if ptype == LO: lo += 1
         else: hi += 1
 
-        if module == "broadcaster":
-            for child in G["broadcaster"]:
-                pulses.append((module, child, ptype))
-        elif module in S_F:
-            if ptype == LO:
-                send_type = LO if S_F[module] else HI
-                for child in G[module]:
-                    pulses.append((module, child, send_type))
-                S_F[module] = not S_F[module]
-        elif module in S_C:
-            S_C[module][sender] = ptype
-            send_type = LO if list(S_C[module].values()).count(HI) == len(S_C[module]) else HI
-            for child in G[module]:
-                pulses.append((module, child, send_type))
-        else:
-            assert module == "rx"
-            if ptype == LO: rx_lo += 1
-            else: rx_hi += 1
+        if sender in C and ptype == HI and C[sender] < 0: C[sender] = i
 
-    k = "".join(['1' if S_F[x] else '0' for x in S_F] + ['1' if list(S_C[x].values()).count(HI) == len(S_C[x]) else '0' for x in S_C])
-    if k in v: print(i, k)
-    v.add(k)
-    if rx_lo == 1 and rx_hi == 0: print("part 2:", i, k); break
+        if module == "roadcaster":
+            for next in M[module]:
+                Q.append((module, next, LO))
+        elif type(S[module]) == bool: # flipflop
+            if ptype == HI: continue
+            S[module] = not S[module]
+            for next in M[module]:
+                Q.append((module, next, S[module]))
+        elif type(S[module]) == dict: # conjunction
+            S[module][sender] = ptype
+            ptype = LO if list(S[module].values()).count(HI) == len(S[module]) else HI
+            for next in M[module]:
+                Q.append((module, next, ptype))
+    if i == 1000: print(lo*hi)
+    if min(C.values()) > 0: print(math.lcm(*C.values())); break
