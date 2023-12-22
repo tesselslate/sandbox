a76727e01ebe5ea1822b60be379e12be3cdb297b
tesselslate <46545045+tesselslate@users.noreply.github.com>
2023-12-22 01:31:41 -0500

    2023: rewrite 22
    this problem really should not have taken an hour god damn

--- 2023/22.py
+++ 2023/22.py
@@ -1,72 +1,49 @@
-import functools, math, re, string, itertools, ul
-from dataclasses import dataclass
-from collections import Counter, defaultdict, deque
+import itertools, ul
+from collections import defaultdict, deque
 
 F = ul.input()
 
-P = []
+B = []
 for l in F:
     a, b, c, x, y, z = ul.scan("%d,%d,%d~%d,%d,%d", l)
     x1, x2 = ul.minmax(a,x)
     y1, y2 = ul.minmax(b,y)
     z1, z2 = ul.minmax(c,z)
-    P.append(((x1,y1,z1),(x2,y2,z2)))
+    B.append(((x1,y1,z1),(x2,y2,z2)))
+B.sort(key=lambda x: x[0][2])
+S = {(x,y,0): -1 for (x,y) in itertools.product(range(10), repeat=2)}
 
-def collide(a,b):
-    x = a[0][0] <= b[1][0] and a[1][0] >= b[0][0]
-    y = a[0][1] <= b[1][1] and a[1][1] >= b[0][1]
-    z = a[0][2] <= b[1][2] and a[1][2] >= b[0][2]
-    return x and y and z
-
-G = [[[0 for _ in range(373)] for _ in range(10)] for _ in range(10)]
-def sim(P):
+for i in range(len(B)):
+    ((x1,y1,z1),(x2,y2,z2)) = B[i]
     while True:
-        moved = False
-        for i in range(len(P)):
-            a = P[i]
-            ((x1,y1,z1),(x2,y2,z2)) = P[i]
-            for b in P: assert not collide(a,b) if a != b else True
-            ok = True
-            z1 -= 1
-            z2 -= 1
-            a = ((x1,y1,z1),(x2,y2,z2))
-            for j in range(len(P)):
-                if i == j: continue
-                b = P[j]
-                if z1 <= 0 or collide(a,b): ok = False; break
-            if ok:
-                P[i] = a
-                moved = True
-        if not moved: return P
+        for (x,y) in itertools.product(range(x1,x2+1), range(y1,y2+1)):
+            if (x,y,z1-1) in S: break
+        else: z1 -= 1; z2 -= 1; continue
+        break
+    for (x, y, z) in itertools.product(range(x1,x2+1),range(y1,y2+1),range(z1,z2+1)):
+        S[x, y, z] = i
 
-from copy import deepcopy
-P2 = deepcopy(P)
-P = sim(P)
-S = defaultdict(set)
-R = defaultdict(set)
-for i in range(len(P)):
-    ((x1,y1,z1),(x2,y2,z2)) = P[i]
-    a = ((x1,y1,z1-1),(x2,y2,z2-1))
-    for j in range(len(P)):
-        if i != j and collide(a, P[j]): S[i].add(j); R[j].add(i)
+G, R = defaultdict(set), defaultdict(set)
+for (x, y, z), i in S.items():
+    if (x, y, z-1) in S:
+        v = S[x, y, z-1]
+        if v != i: G[i].add(v); R[v].add(i)
 
-N = 0
-for i in range(len(P)):
-    for v in S.values():
-        if not v-{i}: break
-    else: N += 1
-print(N)
+S = 0
+for k in G.keys():
+    for dep in R[k]:
+        if not G[dep]-{k}: break
+    else: S += 1
+print(S)
 
-N = 0
-for i in range(len(P)):
-    Q = deque([i])
-    V = set([i])
+S = 0
+for k in G.keys():
+    Q, V = deque([k]), set([k])
     while len(Q):
         i = Q.popleft()
         for k in R[i]:
-            if i in S[k] and not S[k]-V:
+            if i in G[k] and not G[k]-V:
                 Q.append(k)
                 V.add(k)
-    print(i,N,len(V)-1)
-    N += len(V)-1
-print(N)
+    S += len(V)-1
+print(S)
