009af56ee54e232045ad46097a56f8b22b0195da
tesselslate <46545045+tesselslate@users.noreply.github.com>
2023-12-22 13:54:21 -0500

    2023: rewrite 21
    https://github.com/villuna/aoc23/wiki/A-Geometric-solution-to-advent-of-code-2023,-day-21 <- great explanation of the problem and a much cleaner way of doing what i figured out yesterday

--- 2023/21.py
+++ 2023/21.py
@@ -1,6 +1,5 @@
-import functools, math, re, string, itertools, ul
-from dataclasses import dataclass
-from collections import Counter, defaultdict, deque
+import ul
+from collections import deque
 
 F = ul.input()
 G = ul.grid(F)
@@ -9,84 +8,35 @@ sr, sc = 0, 0
 for (r,c) in ul.gridpoints(G):
     if G[r][c] == "S": sr, sc = r, c; break
 
-def occupied(sr, sc, N):
-    V = defaultdict(set)
-    V[0].add((sr,sc))
-    Q = deque([(sr,sc,0)])
-    while len(Q):
-        (r,c,d) = Q.popleft()
-        for rr, cc in ul.padj4():
-            rr, cc = rr+r, cc+c
-            if G[rr%len(G)][cc%len(G)] == "#": continue
-            if (rr,cc) in V[d]: continue
-            V[d].add((rr,cc))
-            if d < N: Q.append((rr,cc,d+1))
-    return V[N-1]
-print(len(occupied(65, 65, 64)))
+V = {(sr,sc): 0}
+Q = deque([(sr,sc,0)])
+while len(Q):
+    (r,c,d) = Q.popleft()
+    for rr, cc in ul.padj4():
+        rr, cc = rr+r, cc+c
+        if not ul.gridcheck(G,rr,cc) or G[rr][cc] == "#": continue
+        if (rr,cc) in V: continue
+        V[rr,cc] = d+1
+        Q.append((rr,cc,d+1))
 
-def n_points(P, r, c):
-    sr, sc = r * 131, c * 131
-    er, ec = sr + 131, sc + 131
-    return len(set((r,c) for (r,c) in itertools.product(range(sr, er), range(sc, ec)) if (r,c) in P))
+print(len([(r,c) for (r,c), d in V.items() if d % 2 == 0 and d <= 64]))
 
-p = occupied(65, 65, 589)
+N = 26501365 // 131
+assert N == 202300
 
-# 589
-x = """
-0 0    0    0    0    0    0    0    0    0    0
-0 0    0    0    928  5526 951  0    0    0    0
-0 0    0    928  6412 7367 6443 951  0    0    0
-0 0    928  6412 7367 7329 7367 6443 951  0    0
-0 928  6412 7367 7329 7367 7329 7367 6443 951  0
-0 5537 7367 7329 7367 7329 7367 7329 7367 5530 0
-0 941  6454 7367 7329 7367 7329 7367 6416 949  0
-0 0    941  6454 7367 7329 7367 6416 949  0    0
-0 0    0    941  6454 7367 6416 949  0    0    0
-0 0    0    0    941  5541 949  0    0    0    0
-0 0    0    0    0    0    0    0    0    0    0
-"""
-# nums = Counter(int(x) for x in re.findall(r'\d+', x))
-# print(nums)
+# https://github.com/villuna/aoc23/wiki/A-Geometric-solution-to-advent-of-code-2023,-day-21
+# My initial solution figured out a similar trick except I had 14 different numbers instead
+# of 4. This is much nicer.
 
-center1     = n_points(p, 0, 0)
-center2     = n_points(p, 0, 1)
-l_corner    = n_points(p, 0, -4)
-r_corner    = n_points(p, 0, 4)
-b_corner    = n_points(p, 4, 0)
-t_corner    = n_points(p, -4, 0)
-o_tl_edge   = n_points(p, -4, -1)
-i_tl_edge   = n_points(p, -3, -1)
-o_tr_edge   = n_points(p, -4, 1)
-i_tr_edge   = n_points(p, -3, 1)
-o_bl_edge   = n_points(p, 4, -1)
-i_bl_edge   = n_points(p, 3, -1)
-o_br_edge   = n_points(p, 4, 1)
-i_br_edge   = n_points(p, 3, 1)
+odd             = len([(r,c) for (r,c), d in V.items() if d % 2 == 1])
+even            = len([(r,c) for (r,c), d in V.items() if d % 2 == 0])
+odd_corners     = len([(r,c) for (r,c), d in V.items() if d % 2 == 1 and d > 65])
+even_corners    = len([(r,c) for (r,c), d in V.items() if d % 2 == 0 and d > 65])
 
-N = 202300
-N2 = N-1
+odd *= (N+1) ** 2
+even *= N ** 2
 
-# n = Counter([l_corner, r_corner, b_corner, t_corner])
-# n[center1] = sum(range(1, N2))*2 - N2
-# n[center2] = sum(range(1, N))*2 - N
-# for x in [o_tl_edge,o_tr_edge,o_bl_edge,o_br_edge]: n[x] = N
-# for x in [i_tl_edge,i_tr_edge,i_bl_edge,i_br_edge]: n[x] = N2
-# print(n)
+odd_corners *= (N+1)
+even_corners *= N
 
-# 1*center2
-# 2*center2, 1*center1
-# 3*center2, 2*center1
-# ...
-# 202300*center2, 202299*center1
-# ...
-# 3*center2, 2*center1
-
-corners = sum([l_corner, r_corner, b_corner, t_corner])
-edges = sum(N * a + N2 * b for (a,b) in zip([o_tl_edge, o_tr_edge, o_bl_edge, o_br_edge], [i_tl_edge, i_tr_edge, i_bl_edge, i_br_edge]))
-center1 = (sum(range(1, N2))*2 + N2) * center1
-center2 = (sum(range(1, N))*2 + N) * center2
-print(corners + edges + center1 + center2)
-
-# 599879935521506 too low
-# 599879937125674 too low
-# 601435117179596 too low
+print(odd + even - odd_corners + even_corners)
