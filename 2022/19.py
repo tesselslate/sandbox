fd51924025cb3b2446f6161d2973fc2c671baa97
tesselslate <46545045+tesselslate@users.noreply.github.com>
2023-12-23 19:03:47 -0500

    2022: improve 19 again
    ~3.3s -> ~0.6s; applied the last main optimization i did not remember from leddoo's video (better upper bound calculation on future geodes)

--- 2022/19.py
+++ 2022/19.py
@@ -12,19 +12,6 @@ for l in F:
         (ge_or, 0, ge_ob, 0)
     ))
 
-# blueprint format
-# (ore bot cost, clay bot cost, ob bot cost, geode bot cost)
-# each "cost" is a tuple containing (ore, clay, ob)
-
-# optimizations:
-# - do not wait and then make a bot if we could have made it before
-# - give up immediately if we cannot beat the best geode count so far
-#
-# - max robots:
-#   - do not make more clay robots than obsidian robot clay cost
-#   - do not make more obsidian robots than geode robot obsidian cost
-#   - do not make more ore robots than max(ore, clay, obsidian, geode)
-
 costs = None
 max_bots = [0] * 4
 GM = 0
@@ -38,22 +25,21 @@ def sim(bots, items, prev, time):
     global GM
 
     if time == 0: return (GM := max(GM, items[3]))
-    if (items[3] + time * (time + bots[3])) < GM: return -1
+    if (items[3] + time*bots[3] + time*(time-1)//2) <= GM: return -1
 
     new_items = tuple(map(lambda a, b: a + b, bots, items))
     M = items[3]
 
-    new_prev = []
+    new_prev = [False] * 4
     for i in range(4):
         can_make = afford(i, items)
-        new_prev.append(can_make)
+        new_prev[i] = can_make or prev[i]
 
         if can_make and not prev[i] and bots[i] < max_bots[i]:
             newer_items = tuple(map(lambda a, b: a - b, new_items, costs[i]))
             new_bots = tuple(x if i != j else x+1 for j, x in enumerate(bots))
             M = max(M, sim(new_bots, newer_items, (0, 0, 0, 0), time - 1))
 
-    # make no bot
     M = max(M, sim(bots, new_items, new_prev, time - 1))
     return M
 
@@ -62,10 +48,10 @@ def run(blueprint, time):
 
     costs = blueprint
     max_bots = [
-        max(blueprint[i][0] for i in range(4)), # max ore robots (max ore for any robot)
-        blueprint[2][1],                        # max clay robots (obsidian clay cost)
-        blueprint[3][2],                        # max obsidian robots (geode obsidian cost)
-        999,                                    # max geode robots (inf)
+        max(blueprint[i][0] for i in range(4)),
+        blueprint[2][1],
+        blueprint[3][2],
+        999,
     ]
     GM = 0
 
