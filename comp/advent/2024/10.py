c7723563cd8eb7b34229fc69b4d83ca3c6a4dc9e
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-10 00:33:15 -0500

    comp/advent/2024: day 10 improvement
    Why did I not realize the distance check is useless. Part 2 is genuinely
    just easier than part 1. I am stupid

--- comp/advent/2024/10.py
+++ comp/advent/2024/10.py
@@ -15,7 +15,7 @@ for (r,c) in ul.gridpoints(G):
         if G[r][c] == 9: S += 1; continue
         for (rr,cc) in ul.padj4():
             np = (r+rr, c+cc)
-            if ul.gridcheck(G, r+rr, c+cc) and G[r+rr][c+cc] == G[r][c] + 1 and np not in V:
+            if ul.gridcheck(G, *np) and G[r+rr][c+cc] == G[r][c] + 1 and np not in V:
                 Q.append(np)
                 V.add(np)
 
@@ -24,13 +24,12 @@ print(S)
 S = 0
 for (r,c) in ul.gridpoints(G):
     if G[r][c] != 0: continue
-    Q = deque([(r,c,0)])
+    Q = deque([(r,c)])
     while len(Q):
-        r, c, d = Q.popleft()
-        if d > 9: continue
+        r, c = Q.popleft()
         if G[r][c] == 9: S += 1; continue
         for (rr,cc) in ul.padj4():
-            np = (r+rr, c+cc, d+1)
-            if ul.gridcheck(G, r+rr, c+cc) and G[r+rr][c+cc] == G[r][c] + 1:
+            np = (r+rr, c+cc)
+            if ul.gridcheck(G, *np) and G[r+rr][c+cc] == G[r][c] + 1:
                 Q.append(np)
 print(S)
