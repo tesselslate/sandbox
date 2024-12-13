05904b326743e5f23126abe6bae5e283cd8fb41e
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-13 02:18:13 -0500

    comp/advent/2024: day 13 improved
    No more Z3, I did the math. Cool. At some point I need to learn the
    linear algebra tricks but I'm happy enough to have solved this without
    Z3, since I've yet to do that for the hailstone problem from 2023.

--- comp/advent/2024/13.py
+++ comp/advent/2024/13.py
@@ -2,23 +2,32 @@ import functools, math, re, string, itertools, ul
 from dataclasses import dataclass
 from collections import Counter, defaultdict, deque
 
-from z3 import *
-
 F = ul.double_linefeed(ul.input())
 
-a = Int("a")
-b = Int("b")
+def solve(ax, ay, bx, by, px, py):
+    # solve the system by elimination:
+    # a*ax + b*bx = px
+    # a*ay + b*by = py
 
-def solve(ax, ay, bx, by, px, py, d):
-    opt = Optimize()
-    opt.minimize(3*a + b)
-    opt.add(a * ax + b * bx == px + d)
-    opt.add(a * ay + b * by == py + d)
-    if opt.check() == sat:
-        model = opt.model()
-        return model[a].as_long() * 3 + model[b].as_long()
+    # coeff * (a*ay + b*by) = coeff * py
+    # a*ax and a*ay cancel out when the equations are added
+    coeff = -ax/ay
+
+    lhs_coeff = bx + coeff * by
+    rhs = px + coeff * py
+
+    b = rhs / lhs_coeff
+    a = (px-b*bx) / ax
+
+    # check if the solution works (a and b are both integers)
+    a, b = round(a), round(b)
+
+    if ax*a + bx*b == px and ay*a + by*b == py:
+        return a * 3 + b
     else:
         return 0
+
+D = 10000000000000
 
 S1, S2 = 0, 0
 for ll in F:
@@ -26,8 +35,8 @@ for ll in F:
     bx, by = ul.ints(ll[1])
     px, py = ul.ints(ll[2])
 
-    S1 += solve(ax, ay, bx, by, px, py, 0)
-    S2 += solve(ax, ay, bx, by, px, py, 10000000000000)
+    S1 += solve(ax, ay, bx, by, px, py)
+    S2 += solve(ax, ay, bx, by, px + D, py + D)
 
 print(S1)
 print(S2)
