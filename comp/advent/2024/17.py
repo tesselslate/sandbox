4444ad9cf2ce4e4ecef5d1410c6fd80d715d3be9
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-17 16:35:41 -0500

    comp/advent/2024: day 17
    5:46 (#175) | 1:11:05 (#350)
    
    This problem was incredibly unsatisfying (at least for me), but
    I generally don't enjoy these reverse engineering problems very much.
    
    I lost one minute on part 1 to forgetting to skip incrementing the PC
    whenever a jnz instruction was encountered. Apart from that it was okay,
    I was mostly just slow at reading.
    
    Part 2 felt incredibly disappointing to me. I had basically all of the
    requisite information to reach a general solution almost immediately
    after disassembling my input program a few minutes in (I saw that it
    was basically all bitwise math, consuming 3 input bits at a time,
    requiring a ~48 bit input to give back a program of the same length),
    but I thought I still needed to understand the actual math in the inner
    loop due to the dependency on the entire value of A.
    
    It turns out that you don't, you can literally just search over each
    octal digit. This was pretty disappointing to figure out after the fact
    since I thought the problem would be really involved or something, but
    it's not. I usually don't enjoy these reverse engineering problems but
    this one felt particularly unenjoyable due to the inner loop's
    complicated nature being a complete red herring.
    
    In any event, my initial attempts at part 2 mostly consisted of brute
    force and trying to figure out Z3. I couldn't get Z3 to work and went
    back to brute forcing, and figured out that it was far more feasible to
    search for an answer by skipping over 8^n solutions at a time. I did
    that for a while until finding a valid input to reproduce my program, at
    which point I just brute forced going back toward 0 to find the lowest
    possible answer.
    
    Hopefully there won't be a 2nd RE problem this year (although there
    usually isn't.)

--- comp/advent/2024/17.py
+++ comp/advent/2024/17.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 from collections import Counter, defaultdict, deque
 
 F = ul.double_linefeed(ul.input())
-_A = ul.ints(F[0][0])[0]
-_B = ul.ints(F[0][1])[0]
-_C = ul.ints(F[0][2])[0]
+A = ul.ints(F[0][0])[0]
+B = ul.ints(F[0][1])[0]
+C = ul.ints(F[0][2])[0]
 
 PROG = ul.ints(F[1][0])
 
@@ -13,12 +13,11 @@ def run(A, B, C):
     out = []
 
     def evalcombo(operand):
-        if 0 <= operand <= 3:
-            return operand
-        elif operand == 4: return A
-        elif operand == 5: return B
-        elif operand == 6: return C
-        else: raise ValueError()
+        if 0 <= operand <= 3: return operand
+        elif    operand == 4: return A
+        elif    operand == 5: return B
+        elif    operand == 6: return C
+
     PC = 0
     while PC < len(PROG):
         instr = PROG[PC]
@@ -31,14 +30,10 @@ def run(A, B, C):
         elif instr == 2: # bst
             B = evalcombo(operand) % 8
         elif instr == 3: # jnz
-            if A != 0:
-                # print(A)
-                PC = operand; continue
+            if A != 0: PC = operand; continue
         elif instr == 4: # bxc
-            # read but ignore operand
             B ^= C
         elif instr == 5: # out
-            # print("out, ", evalcombo(operand) % 8)
             out.append(evalcombo(operand) % 8)
         elif instr == 6: # bdv
             B = A // (2 ** evalcombo(operand))
@@ -48,61 +43,12 @@ def run(A, B, C):
         PC += 2
     return out
 
-S = 88920464920329
-assert S > 8**15
-for i in range(109020022673161, S, -1):
-    out = run(i, _B, _C)
-    if out == PROG: print(i)
+print(",".join([str(x) for x in run(A, B, C)]))
 
-"""
-[2, 4, 1, 5, 7, 5, 0, 3, 4, 1, 1, 5, 4, 0, 5, 1]
-[2, 4, 1, 5, 7, 5, 0, 3, 4, 1, 1, 6, 5, 5, 3, 0]
-- 88964052127497 
-
-
-109020022673161
-
-
-
-
-
-Register A: 47719761
-Register B: 0
-Register C: 0
-
-Program: 2,4,1,5,7,5,0,3,4,1,1,6,5,5,3,0
-
-2,4 bst A       B = A%8
-1,5 bxl         B ^= 5
-7,5 cdv         C = A // (2^B)
-0,3 adv         A = A // 8
-4,1 bxc         B ^= C
-1,6 bxl         B ^= 6
-5,5 out !       print(B % 8)
-3,0 jnz !
-
-B = A%8
-C = A // (2**(B^5))
-A = A // 8
-B ^= 3 ^ C
-print(B%8)
-
-
-B = 4
-print(2)
-
-loop needs to run 16 times
-A decreases by about 8 each time
-
-
-88920011934106  2415751556304632
-88920011935489  2415751556307401
-88920011935497  2415751556307411
-88920028645786  2415751656104632
-88920028647169  2415751656107401
-88920028647177  2415751656107411
-88920028672777
-88920028711322
-88920028712705
-88920028712713
-"""
+def dfs(A):
+    for j in range(8):
+        out = run(A * 8 + j, B, C)
+        if out == PROG[len(PROG)-len(out):]:
+            if out == PROG: print(A * 8 + j); exit()
+            dfs(A * 8 + j)
+dfs(0)
