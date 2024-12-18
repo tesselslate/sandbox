aad8836b363280c79ffa7cf6aef402fb1f8052da
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-18 00:26:50 -0500

    comp/advent/2024: day 18
    3:51 (#126) | 6:05 (#108)
    
    Best average rank without a top 100 I've had I think? Pretty chill
    problem.
    
    Lost a little bit of time to the wording on part 1 (I thought "falling"
    initially meant the obstructions would move over time), and then a
    little over a minute to not ensuring X and Y stayed within [0, 70].
    
    Lost another minute on part 2 to misunderstanding the description.
    I misread it and thought it meant "figure out any point that blocks the
    path," which is a pretty boring question. I managed to fix the mistake
    and get the correct answer before the submission timeout ended.
    
    Would have been nice to score some points today but not bad.

--- comp/advent/2024/18.py
+++ comp/advent/2024/18.py
@@ -2,38 +2,34 @@ import functools, math, re, string, itertools, ul
 from dataclasses import dataclass
 from collections import Counter, defaultdict, deque
 
-OF = ul.input()
+F = ul.input()
 S = 0
 
-F = OF[:1024]
+O = {tuple(ul.ints(l)) for l in F[:1024]}
 
-P = set()
-start = (0,0)
-end = (70,70)
-
-for l in F:
-    x, y = ul.ints(l)
-    P.add((x,y))
-
-def path(P):
-    Q = deque([(start, 0)])
+def pathfind(O):
+    Q = deque([((0, 0), 0)])
     V = set()
+
     while len(Q):
         pos, cost = Q.popleft()
-        x,y = pos
+        x, y = pos
 
-        if pos == end:
+        if pos == (70, 70):
             return cost
 
         for (dx, dy) in ul.padj4():
             nx, ny = x+dx, y+dy
 
-            if (nx,ny) not in P and (nx,ny) not in V and 0 <= nx <= 70 and 0 <= ny <= 70:
+            if (nx,ny) not in O and (nx,ny) not in V and 0 <= nx <= 70 and 0 <= ny <= 70:
                 V.add((nx,ny))
                 Q.append(((nx,ny),cost+1))
 
-for l in OF[1024:]:
-    r,c = ul.ints(l)
-    if (r,c) in P: continue
-    P.add((r,c))
-    if path(P) == None: print((r,c)); exit()
+print(pathfind(O))
+
+for l in F[1024:]:
+    O.add(tuple(ul.ints(l)))
+
+    if pathfind(O) == None:
+        print(l)
+        exit()
