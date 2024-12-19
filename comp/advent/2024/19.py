af00ea51fde9477bd23e9ce1685067cd0c3c0150
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-19 00:15:38 -0500

    comp/advent/2024: day 19
    1:39 (#80) | 2:09 (#45)
    
    No real complaints, best day of the year by far (and I think my best
    overall performance ever, basically?). Only timelosses were to verifying
    my towel parsing was correct and to needing to fix a syntax error on
    part 2. Other than that it was just general slowness from reading the
    problem and making typos.

--- comp/advent/2024/19.py
+++ comp/advent/2024/19.py
@@ -3,22 +3,15 @@ from dataclasses import dataclass
 from collections import Counter, defaultdict, deque
 
 F = ul.double_linefeed(ul.input())
-S = 0
 
 towels = F[0][0].replace(",", "").split()
 patterns = F[1]
-print(towels)
 
 @functools.cache
 def dfs(pattern):
     if pattern == "": return 1
-    s = 0
-    for towel in towels:
-        if pattern.startswith(towel):
-            s += dfs(pattern[len(towel):])
-    return s
 
-for pattern in patterns:
-    S += dfs(pattern)
+    return sum(dfs(pattern[len(towel):]) for towel in towels if pattern.startswith(towel))
 
-print(S)
+print(sum(dfs(pattern) > 0 for pattern in patterns))
+print(sum(dfs(pattern) for pattern in patterns))
