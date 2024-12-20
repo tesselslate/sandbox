59d69b828ccb3a018f302c3104b097b68ac0626d
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-20 01:49:14 -0500

    comp/advent/2024: day 20
    35:52 (#1784) | 53:34 (#1162).
    
    Meh, not sure how to feel about this problem. I spent a lot of time
    attempting to bruteforce part 1, and it's still not clear to me why it
    didn't work, but oh well.
    
    The inputs for this problem are also... seemingly not great? I had
    2 fairly major bugs in my initial part 1 solve which didn't cause
    any issues, but *did* cause issues on part 2.
    
    I'm also not a fan of how annoying it is to debug this problem and how
    much it felt like twiddling with magic constants until it worked.

--- comp/advent/2024/20.py
+++ comp/advent/2024/20.py
@@ -5,57 +5,40 @@ from collections import Counter, defaultdict, deque
 F = ul.input()
 G = ul.grid(F)
 
-start,end=None,None
+start = None
 for (r,c) in ul.gridpoints(G):
-    if G[r][c]=="S":start=(r,c)
-    if G[r][c]=="E":end=(r,c)
+    if G[r][c] == "S":
+        start = (r,c)
+        break
 
-Q = deque([(start,0)])
-V = defaultdict(lambda: math.inf)
-V[start] = 0
-
-mt = 0
-while len(Q):
-    pos,time = Q.popleft()
-
-    if pos == end: mt = time; break
-
-    r,c=pos
-    for (rr,cc) in ul.padj4():
-        nr,nc = r+rr,c+cc
-
-        if G[nr][nc] != "#":
-            if V[nr,nc] < math.inf: continue
-            V[nr,nc]=time+1
-            Q.append(((nr,nc),time+1))
-
-adj = {}
-Q = deque([((0,0),0)])
+Q = deque([(start, 0)])
+V = defaultdict(lambda: math.inf, {start: 0})
 while len(Q):
     pos, time = Q.popleft()
-    if time == 20: continue
-    r,c=pos
-    for (rr,cc) in ul.padj4():
-        nr,nc=r+rr,c+cc
-        if (nr,nc) in adj: continue
-        adj[nr,nc] = time
-        Q.append(((nr,nc),time+1))
+    r, c = pos
 
-print(len(adj))
+    for (rr, cc) in ul.padj4():
+        nr, nc = r+rr, c+cc
 
-S = 0
-C = Counter()
-for pos, time in V.items():
-    r,c = pos
-    for (rr,cc) in adj:
-        nr,nc = r+rr,c+cc
-        if not (nr,nc) in V: continue
-        td = V[nr,nc] - time - adj[rr,cc]
-        if td > 100: C[td] += 1
+        if G[nr][nc] != "#" and (nr, nc) not in V:
+            V[nr, nc] = time + 1
+            Q.append(((nr, nc), time + 1))
 
-for k in sorted(C.keys()):
-    print(k, C[k])
-print(sum(C.values()))
+def cheats(clen):
+    cadj = {}
+    for (r, c) in itertools.product(range(-clen, clen+1), repeat=2):
+        if abs(r) + abs(c) <= clen:
+            cadj[r, c] = abs(r) + abs(c)
 
-#test:285
-# not 1238075
+    S = 0
+    for pos, time in V.items():
+        r, c = pos
+        for (dr, dc), dt in cadj.items():
+            nr, nc = r+dr, c+dc
+            if (nr, nc) in V and V[nr, nc] - time - dt >= 100:
+                S += 1
+
+    return S
+
+print(cheats(2))
+print(cheats(20))
