f0ac6645c470d3d937e34c55a751f6a756e3127a
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-21 14:47:44 -0500

    comp/advent/2024: day 21
    59:58 (#395) | 14:03:32 (#5693)
    
    First time I've ever given up and gone to bed when attempting to solve
    a problem at midnight.
    
    Not much to say, this one was pretty difficult and did not mesh well
    with my brain.

--- comp/advent/2024/21.py
+++ comp/advent/2024/21.py
@@ -4,101 +4,63 @@ from collections import Counter, defaultdict, deque
 
 F = ul.input()
 
-numpad = {
-    (0,0): "7",
-    (0,1): "8",
-    (0,2): "9",
-    (1,0): "4",
-    (1,1): "5",
-    (1,2): "6",
-    (2,0): "1",
-    (2,1): "2",
-    (2,2): "3",
-    (3,1): "0",
-    (3,2): "A",
-}
+def mkgrid(grid):
+    return {c: (i//3, i%3) for i, c in enumerate(grid) if c != " "}
 
-dirpad = {
-    (0,1): "^",
-    (0,2): "A",
-    (1,0): "<",
-    (1,1): "v",
-    (1,2): ">",
-}
-
-arrows = {
-    (0,1): ">",
-    (0,-1): "<",
-    (1,0): "v",
-    (-1,0): "^",
-    (0,0): "",
-}
-
-def revextend(d):
-    nd = {}
-    for k, v in d.items():
-        nd[v] = k
-        nd[k] = v
-    return nd
-
-numpad = revextend(numpad)
-dirpad = revextend(dirpad)
+numpad = mkgrid("789456123 0A")
+dirpad = mkgrid(" ^A<v>")
 
 def path_from(src, dst, pad):
-    sr, sc = src
-    dr, dc = dst
-    h = (0, ul.sign(dc-sc))
-    v = (ul.sign(dr-sr), 0)
+    sr, sc = pad[src]
+    dr, dc = pad[dst]
+
+    h = ["", ">", "<"][ul.sign(dc-sc)]
+    v = ["", "v", "^"][ul.sign(dr-sr)]
 
     P = set()
 
-    if (sr, dc) in pad:
-        P.add(arrows[h] * abs(sc-dc) + arrows[v] * abs(sr-dr))
-    if (dr, sc) in pad:
-        P.add(arrows[v] * abs(sr-dr) + arrows[h] * abs(sc-dc))
+    if (sr, dc) in pad.values(): P.add(h * abs(sc-dc) + v * abs(sr-dr))
+    if (dr, sc) in pad.values(): P.add(v * abs(sr-dr) + h * abs(sc-dc))
 
     return P
 
 def gen_pad_paths(src, code, pad):
-    paths = []
-    for a, b in itertools.pairwise(pad[src] + code):
-        paths.append(path_from(pad[a], pad[b], pad))
+    paths = [
+        path_from(a, b, pad)
+        for a, b in itertools.pairwise(src + code)
+    ]
 
-    full_paths = list(itertools.product(*paths))
-    return ["A".join(path) + "A" for path in full_paths]
+    return ["A".join(path) + "A" for path in itertools.product(*paths)]
 
 @functools.cache
 def moves(src, dst, depth):
-    if depth == 1:
+    if depth == 0:
         return 1
 
     paths = path_from(src, dst, dirpad)
     best = math.inf
 
     for path in paths:
-        n = 0
-        for a, b in itertools.pairwise("A" + path + "A"):
-            n += moves(dirpad[a], dirpad[b], depth - 1)
-
+        n = sum(
+            moves(a, b, depth - 1)
+            for a, b in itertools.pairwise("A" + path + "A")
+        )
         best = min(best, n)
 
     return best
 
-S = 0
-for code in F:
-    numpad_paths = gen_pad_paths(numpad["A"], code, numpad)
+def complexity(code, depth):
+    paths = gen_pad_paths("A", code, numpad)
     best = math.inf
 
-    for path in numpad_paths:
-        n = 0
-        print(path)
-        for i, char in enumerate(path):
-            if i == 0: prev = dirpad["A"]
-            else: prev = dirpad[path[i-1]]
-            n += moves(prev, dirpad[char], 26)
-
+    for path in paths:
+        n = sum(
+            moves(a, b, depth)
+            for a, b in itertools.pairwise("A" + path)
+        )
         best = min(best, n)
 
-    print(code, best)
-    S += ul.ints(code)[0] * best
-print(S)
+    return int(code[:3]) * best
+
+print(sum(complexity(l, 2) for l in F))
+print(sum(complexity(l, 25) for l in F))
