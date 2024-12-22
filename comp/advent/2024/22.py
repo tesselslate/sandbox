ad6b80741133fa48fb635f232524d8bbb2a52823
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-22 02:12:00 -0500

    comp/advent/2024: day 22
    2:47 (#135) | 35:22 (#1091)
    
    Small reading errors on part 1 led to a wrong submission, so could have
    made top 100 there. I wasted a bit of time attempting to brute-force
    part 2 but ended up losing far more time to, once again, not reading the
    problem correctly.
    
    That being said, I do think the problem statement for part 2 is pretty
    messy relative to all of the other problems so far this year. The
    sequences of price differences are pretty terrible, since one of them
    ends up placing equal prices on both ends, and the other only has
    a difference of 1 before and after the sequence. The initial wording of
    when to sell is extremely unhelpful and it's not clear what it means
    unless you read the entirety of the following paragraph.

--- comp/advent/2024/22.py
+++ comp/advent/2024/22.py
@@ -3,84 +3,37 @@ from dataclasses import dataclass
 from collections import Counter, defaultdict, deque
 
 F = ul.input()
-S = 0
 
-def mix(n, secret):
-    return n ^ secret
+def shash(x):
+    def pmix(x, y):
+        return (x ^ y) % 16777216
 
-def prune(n):
-    return n % 16777216
+    x = pmix(x << 6, x)
+    x = pmix(x >> 5, x)
+    x = pmix(x << 11, x)
+    return x
 
-def ones(n):
-    return n % 10
-
-price_changes = []
-prices = []
+prices, pdiffs = [], []
 for l in F:
     n = int(l)
-    first = [ones(n)]
-    pc = []
-    p = [n]
+
+    p = []
     for i in range(2000):
-        n = prune(mix(n * 64, n))
-        n = prune(mix(n // 32, n))
-        n = prune(mix(n * 2048, n))
-        first.append(ones(n))
+        n = shash(n)
         p.append(n)
-    # print(l, n)
-    # S += n
-    for a, b in itertools.pairwise(first):
-        pc.append(b-a)
-    price_changes.append(pc)
+
+    pd = [(b%10) - (a%10) for a, b in itertools.pairwise(p)]
     prices.append(p)
+    pdiffs.append(pd)
 
-TARGET = (-2,1,-1,3)
 C = Counter()
-for i in range(len(F)):
-    C2 = Counter()
-    for j in range(1, len(price_changes[i]) - 4):
-        cc = tuple(price_changes[i][j:j+4])
-        if cc in C2: continue
-        C2[cc] += prices[i][j+4] % 10
+for p, pd in zip(prices, pdiffs):
+    seen = set()
+    for i in range(len(pd)-4):
+        diffset = tuple(pd[i:i+4])
+        if diffset not in seen:
+            C[diffset] += p[i+4] % 10
+            seen.add(diffset)
 
-        if cc == TARGET: print(F[i], prices[i][j+4])
-    for k, v in C2.items():
-        C[k] += v
-
-print(C[(-2,1,-1,3)])
-
-best = 0
-for k, v in C.items():
-    if v > best:
-        best = v
-        print(k, best)
-#
-#
-# def check(pcseq, seq):
-#     for i in range(len(seq)-len(pcseq)):
-#         if seq[i:i+len(pcseq)] == list(pcseq): return i
-#     return None
-#
-# @functools.cache
-# def tryseq(pcseq):
-#     S = 0
-#     for k, l in enumerate(F):
-#         idx = check(pcseq, price_changes[k])
-#         if idx is None: continue
-#         selli = idx
-#
-#         n = int(l)
-#         S += (prices[k][selli] + 1)%10
-#     return S
-#
-# best = 0
-# for _i in range(len(price_changes)):
-#     for i in range(len(price_changes[_i])-4):
-#         pcseq = price_changes[_i][i:i+4]
-#
-#         S = tryseq(tuple(pcseq))
-#         if S > best: print(S, pcseq)
-#         best = max(S,best)
-# print(best)
-#
-# # not 1529
+print(sum(xs[-1] for xs in prices))
+print(max(C.values()))
