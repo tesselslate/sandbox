66a9f593b08e98693f064040336bf1e3628d60c2
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-24 19:51:16 -0500

    comp/advent/2024: day 24 part 2 cleanup
    This is super brittle but it works on at least 2 inputs!

--- comp/advent/2024/24.py
+++ comp/advent/2024/24.py
@@ -6,6 +6,7 @@ F = ul.double_linefeed(ul.input())
 
 bits = {}
 gates = {}
+gates_bw = defaultdict(list)
 
 for l in F[0]:
     name, value, *_ = l.split()
@@ -14,6 +15,8 @@ for l in F[0]:
 for l in F[1]:
     a, op, b, _, dst, *_ = l.split()
     gates[dst] = (a, op, b)
+    gates_bw[a].append((a, op, b, dst))
+    gates_bw[b].append((a, op, b, dst))
 
 @functools.cache
 def evaluate(dst):
@@ -29,3 +32,30 @@ def evaluate(dst):
 
 zbits = [evaluate(f"z{i:#02}") for i in reversed(range(46))]
 print(int("".join(str(z) for z in zbits), base=2))
+
+# dkr,ggk,hhh,htp,rhv,z05,z15,z20
+# colors = {"AND": "red", "OR": "green", "XOR": "blue"}
+
+swaps = []
+
+bad_z = []
+for i in range(1,45):
+    if gates[f"z{i:#02}"][1] != "XOR":
+        bad_z.append(i)
+
+for i in range(1,45):
+    # one instance of swapped XOR/AND outputs
+    gates = gates_bw[f"x{i:#02}"]
+    bxor = [x for x in gates if x[1] == "XOR"][0]
+    band = [x for x in gates if x[1] == "AND"][0]
+
+    bxor_gates = gates_bw[bxor[3]]
+    if len(bxor_gates) != 2:
+        swaps += [bxor[3], band[3]]
+
+    # three instances of messed up Z gates
+    if i in bad_z:
+        bxor2 = [x for x in bxor_gates if x[1] == "XOR"][0]
+        swaps += [f"z{i:#02}", bxor2[3]]
+
+print(",".join(sorted(swaps)))
