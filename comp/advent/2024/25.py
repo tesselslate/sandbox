01fe56f63d437052b2f4e5a0cbd5f70c22c5a44c
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-25 00:29:38 -0500

    comp/advent/2024: day 25

--- comp/advent/2024/25.py
+++ comp/advent/2024/25.py
@@ -3,38 +3,11 @@ from dataclasses import dataclass
 from collections import Counter, defaultdict, deque
 
 F = ul.double_linefeed(ul.input())
-GS = [ul.grid(x) for x in F]
+G = [ul.grid(ll) for ll in F]
 
-locks = []
-keys = []
-
-for G in GS:
-    if all(x == "#" for x in G[0]) and all(x == "." for x in G[-1]):
-        locks.append(G)
-    else:
-        keys.append(G)
-
-nlocks = []
-nkeys = []
-for lock in locks:
-    heights = []
-    for i in range(len(lock[0])):
-        col = ul.gcol(lock, i)
-        heights.append(col.index(".")-1)
-    nlocks.append(tuple(heights))
-
-for key in keys:
-    heights = []
-    for i in range(len(key[0])):
-        col = ul.gcol(key, i)
-        heights.append(col.count("#") - 1)
-    nkeys.append(tuple(heights))
-
-pairs = set()
-for lock in nlocks:
-    for key in nkeys:
-        print(lock,key)
-        for a, b in zip(lock, key):
-            if a+b > 5: break
-        else: pairs.add((lock, key))
-print(len(pairs))
+S = 0
+for a, b in itertools.product(G, repeat=2):
+    for (r, c) in ul.gridpoints(a):
+        if a[r][c] == b[r][c] == "#": break
+    else: S += 1
+print(S // 2)
