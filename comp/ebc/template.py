889174ce17a1ecd2b8c0f19750f28c901611a7dd
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-26 23:42:26 -0500

    comp: factor out python template/utilities

--- comp/ebc/template.py
+++
@@ -1,8 +0,0 @@
-import functools, math, re, string, itertools, ul
-from dataclasses import dataclass
-from collections import Counter, defaultdict, deque
-
-F = ul.input()
-
-for l in F:
-    print(l)
