0c2c5e4b06c1318c5964a29b420e865751cdd1c4
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-30 18:30:02 -0500

    comp/codeforces: add template

---
+++ comp/codeforces/template.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+void test_case() {
+
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) test_case();
+}
